bdbab0e93acb51a424735430a3575da8bbeeb3e1
charletontech <ukacharlie@gmail.com>
2025-05-12 23:05:39 +0100

    94% done

---
+++ scripts/forgetPassword.js
@@ -0,0 +1,62 @@
+import { QuestZender, toast, url } from "./utils.js";
+const loginForm = document.getElementById("loginForm");
+const loginIdentifierInput = document.getElementById("loginIdentifier");
+const loginIdentifierError = document.getElementById("loginIdentifierError");
+const submitBtn = document.getElementById("submitBtn");
+
+loginForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  let isValid = true;
+
+  if (!loginIdentifierInput.value.trim()) {
+    loginIdentifierError.innerText = "Please enter your email or username.";
+    isValid = false;
+  } else {
+    loginIdentifierError.textContent = "";
+  }
+
+  if (isValid) {
+    const payload = {
+      identifier: loginIdentifierInput.value,
+    };
+
+    // alert user to wait
+    toast(
+      "info",
+      "Verifying...",
+      "Lets check if your email exists then send you an OTP."
+    );
+    submitBtn.innerText = "Requesting OTP...";
+    submitBtn.disabled = true;
+
+    // Send request to login api
+    try {
+      const response = await QuestZender(
+        url() + "/auth/forgot-password",
+        "POST",
+        JSON.stringify(payload)
+      );
+
+      var message = await response.json();
+
+      if (!response.ok) {
+        toast("info", "Oops!", message.error);
+        return;
+      }
+
+      // redirect user to change password page
+      toast(
+        "success",
+        "OTP sent!",
+        "We found your account. We have sent an OTP to your email..."
+      );
+      window.location = "/change-password";
+    } catch (error) {
+      console.log(error);
+      toast("error", "Error", error);
+    } finally {
+      submitBtn.innerText = "Log In";
+      submitBtn.disabled = false;
+    }
+  }
+});
